09cc24086088f5dff3240699cacfc3c1ed037748
Shajaan Balasingam <shajaan_71@hotmail.com>
2022-05-02 14:59:00 -0400

    Ajout des interfaces + internalisation

--- app/src/main/java/com/example/tp3_appnat2/MenuActivity.java
+++ app/src/main/java/com/example/tp3_appnat2/MenuActivity.java
@@ -3,12 +3,17 @@ package com.example.tp3_appnat2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.menu);
+    }
+
+    public void quitter(View view) {
+        System.exit(0);
     }
 }
