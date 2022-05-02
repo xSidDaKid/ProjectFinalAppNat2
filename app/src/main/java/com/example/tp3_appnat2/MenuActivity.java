a692affda72078040338438cd5a92f972d1f0c8d
Shajaan Balasingam <shajaan_71@hotmail.com>
2022-05-02 16:11:45 -0400

    Ajout des redirections

--- app/src/main/java/com/example/tp3_appnat2/MenuActivity.java
+++ app/src/main/java/com/example/tp3_appnat2/MenuActivity.java
@@ -6,6 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+/**
+ * @Author Alperen Albaz, Shajaan Balasingam et Gafran Ijaz
+ * @Groupe 02
+ * @Remis_a Toufik Bellal
+ * @Cours 420-G25-RO
+ * @Date_de_remise 20 mai 2022
+ * Classe qui fait les redirections du menu
+ */
 public class MenuActivity extends AppCompatActivity {
 
     @Override
@@ -14,12 +22,42 @@ public class MenuActivity extends AppCompatActivity {
         setContentView(R.layout.menu);
     }
 
-    public void quitter(View view) {
-        System.exit(0);
+    /**
+     * Menu Option #1
+     * Jouer contre un AI
+     * @param view
+     */
+    public void playerVSAI(View view) {
+        Intent intent = new Intent(this, unJoueurActivity.class);
+        startActivity(intent);
     }
 
+    /**
+     * Menu Option #2
+     * Joeur contre un autre joueur
+     * @param view
+     */
+    public void playerVsPlayer(View view) {
+        Intent intent = new Intent(this, IdentificationActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Menu Option #3
+     * Page d'info
+     * @param view
+     */
     public void about(View view) {
         Intent intent = new Intent(this, AboutActivity.class);
         startActivity(intent);
+    }
+
+    /**
+     * Menu Option #4
+     * Quitter
+     * @param view
+     */
+    public void quitter(View view) {
+        System.exit(0);
     }
 }
---
+++ app/src/main/java/com/example/tp3_appnat2/unJoueurActivity.java
@@ -0,0 +1,14 @@
+package com.example.tp3_appnat2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class unJoueurActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_un_joueur);
+    }
+}
