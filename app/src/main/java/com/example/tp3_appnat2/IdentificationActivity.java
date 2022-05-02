c6465adbe17154e33fb986df90be7676e29966c7
Shajaan Balasingam <shajaan_71@hotmail.com>
2022-05-02 17:53:19 -0400

    Ajout du nom des joueurs dans la fenetre de jeu

--- app/src/main/java/com/example/tp3_appnat2/IdentificationActivity.java
+++ app/src/main/java/com/example/tp3_appnat2/IdentificationActivity.java
@@ -2,13 +2,30 @@ package com.example.tp3_appnat2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class IdentificationActivity extends AppCompatActivity {
+    EditText joueur1, joueur2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_identification);
+
+    }
+
+    public void jouer(View view) {
+        joueur1 = findViewById(R.id.joueur1);
+        joueur2 = findViewById(R.id.joueur2);
+        String nomJoueur1 = joueur1.getText().toString();
+        String nomJoueur2 = joueur2.getText().toString();
+
+        Intent i = new Intent(this, JeuActivity.class);
+        i.putExtra("nomJoueurUn", nomJoueur1);
+        i.putExtra("nomJoueurDeux", nomJoueur2);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/tp3_appnat2/JeuActivity.java
+++ app/src/main/java/com/example/tp3_appnat2/JeuActivity.java
@@ -2,13 +2,29 @@ package com.example.tp3_appnat2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class JeuActivity extends AppCompatActivity {
+    TextView scoreJoueur1, scoreJoueur2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jeu);
+    }
+
+    public void init() {
+        Intent i2 = getIntent();
+        String p1 = i2.getStringExtra("nomJoueurUn");
+        String p2 = i2.getStringExtra("nomJoueurDeux");
+
+        scoreJoueur1 = findViewById(R.id.scoreJoeur1);
+        scoreJoueur2 = findViewById(R.id.scoreJoeur2);
+
+        scoreJoueur1.append(p1);
+        scoreJoueur2.append(p2);
     }
 }
